312c919018f2d302638c52a9c25fd06880727c48
Alvin Jaison <alvinjaison@outlook.com>
2022-05-25 11:09:41 +0530

    Update finder.sh

--- finder.sh
+++ finder.sh
@@ -9,34 +9,36 @@ for region in $RegionList
     do
     echo $region
     ec2RI=$(aws ec2 describe-reserved-instances --filters Name=state,Values=active --region $region --query 'ReservedInstances[*].ReservedInstancesId' --output                                                                              text)
-    rdsRI=$(aws rds describe-reserved-db-instances  --query 'ReservedDBInstances[*].LeaseId' --region us-east-1 --output text)
-    redShiftRN=$(aws redshift describe-reserved-nodes  --region us-east-1 --query 'ReservedNodes[*].ReservedNodeId' --output text)
-    esRN=$(aws elasticache describe-reserved-cache-nodes  --region us-east-1 --query 'ReservedCacheNodes[*].ReservedCacheNodesOfferingId' --output text)
+    rdsRI=$(aws rds describe-reserved-db-instances  --query 'ReservedDBInstances[*].LeaseId' --region $region --output text)
+    redShiftRN=$(aws redshift describe-reserved-nodes  --region $region --query 'ReservedNodes[*].ReservedNodeId' --output text)
+    esRN=$(aws elasticache describe-reserved-cache-nodes  --region $region --query 'ReservedCacheNodes[*].ReservedCacheNodesOfferingId' --output text)
     if [[ -z "$ec2RI" ]]; then
         echo -e "No reserved instance found in $region"
     else
         echo -e "Reserved instances found in $region  are:\n $ec2RI"
-    #echo $ec2RI >> list.txt
+        echo $ec2RI >> list.txt
     fi
 
     if [[ -z "$rdsRI" ]]; then
         echo -e "No reserved DB instances found in $region"
     else
         echo -e "Reserved DB instances found in $region are:\n $rdsRI"
-        #echo $rdsRI >> list.txt
+        echo $rdsRI >> list.txt
     fi
 
     if [[ -z "$redShiftRN" ]]; then
         echo -e "No Redshift reserved nodes found in $region"
     else
         echo -e "Redshift reserved nodes found in $region are:\n $redShiftRN"
-        #echo $redshiftRN >> list.txt
+        echo $redshiftRN >> list.txt
     fi
 
     if [[ -z "$esRN" ]]; then
         echo -e "No ElasticSearch reserved nodes found found in $region"
     else
         echo -e "ElasticSearch reserved nodes found in $region are:\n $esRN"
-        #echo $esRN >> list.txt
+        echo $esRN >> list.txt
     fi
+    
+    cat list.txt
     done
